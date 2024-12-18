0cbb31ed1786d9faec6d7c923d9c30f188f11790
Tan Yi Peng <61421451+tanyipeng834@users.noreply.github.com>
2024-12-18 17:43:38 +0800

    finished with caesar

---
+++ cs50/caesar.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include<string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+char rotate(char c, int key);
+int main(int argc, char* argv[]){
+
+   
+    if(argc!=2){
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    for(int i =0;i <strlen(argv[1]);i++){
+        if(!isdigit(argv[1][i])){
+            printf("Usage: ./caesar key\n");
+            return 1;
+
+        }
+
+        
+    }
+    int key  = atoi(argv[1]);
+    char *text = (char *)malloc(1000 * sizeof(char));
+    printf("PlainText: ");
+   fgets(text, 1000, stdin);
+   char * outputText = (char *)malloc(1000 * sizeof(char));
+   int len = strlen(text);
+
+   for(int i =0;i<len;i++){
+    outputText[i] = rotate(text[i],key);
+   }
+    outputText[len] = '\0';
+
+   printf("Ciphertext: %s", outputText);
+   free(text);
+   free(outputText);
+
+
+
+    
+
+
+
+
+
+}
+
+char rotate(char c , int key)
+{
+    
+    if(isalpha(c)){
+        if(islower(c)){
+            int result = ((c-97) +key)%26 +97  ;
+            return (char)result;
+
+        }
+        else if(isupper(c)){
+            int result = ((c-65) +key)%26 +65 ;
+            return (char)result;
+
+
+        }
+    }
+    else{
+        return c;
+    }
+
+    
+
+}
